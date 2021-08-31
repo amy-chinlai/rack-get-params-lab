e6deef591d9851ac2380f8c6825704a6482ec127
amy-chinlai <amy@actionnetwork.org>
2021-08-31 13:51:58 -0400

    commit tree 2037db3daf29a9c7dfe7aa62e2fc9fe56ff6ef61

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = ["Mouthwash", "Toner", "Treats"]
 
   def call(env)
     resp = Rack::Response.new
@@ -10,6 +11,18 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      # resp.write handle_cart
+      if @@cart == []
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      resp.write handle_add(item)
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
@@ -25,6 +38,16 @@ class Application
       return "#{search_term} is one of our items"
     else
       return "Couldn't find #{search_term}"
+    end
+  end
+
+
+  def handle_add(item)
+    if @@items.include?(item)
+      @@cart << item
+      return "added #{item}"
+    else
+      return "We don't have that item"
     end
   end
 end
